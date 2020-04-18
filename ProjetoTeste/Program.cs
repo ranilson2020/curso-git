e98a84598225354e9e8a4d7ceeef90999e190c74
Ranilson Silva <ranilson.tec@gmail.com>
2020-04-18 13:58:22 -0300

    Projeto Exibe a frase Ola Mundo

---
+++ ProjetoTeste/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ProjetoTeste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá Mundo!");
+            Console.ReadLine();
+        }
+    }
+}
