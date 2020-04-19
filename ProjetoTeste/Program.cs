9f5bb4bf380d783ff1d56ffd634b106b80349f3f
Ranilson Silva <ranilson.analista2020@hotmail.com>
2020-04-19 12:48:40 -0300

    Coloquei meu telefone e uns traços

--- ProjetoTeste/Program.cs
+++ ProjetoTeste/Program.cs
@@ -6,11 +6,14 @@ namespace ProjetoTeste
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("----------------------------------------------------------------");
             Console.WriteLine("Olá Mundo!");
             Console.WriteLine("Bom Dia!");
             Console.WriteLine("Como vc esta hoje?");           
             Console.WriteLine("Primeiro Projeto enviado para GitHub");
             Console.WriteLine("Meu E-mail é ranilson.analista2020@hotmail.com");
+            Console.WriteLine("Meu Celular (79)99170-4949");
+            Console.WriteLine("----------------------------------------------------------------");
             Console.ReadLine();
         }
     }
